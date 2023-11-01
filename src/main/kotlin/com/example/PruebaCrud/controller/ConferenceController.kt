78765acb3a91d154abd1d62054c2595a231e0dab
Wilson Castillo <115800617+Wilson8jsn@users.noreply.github.com>
2023-11-01 09:24:15 -0500

    commid

--- src/main/kotlin/com/example/PruebaCrud/controller/ConferenceController.kt
+++ src/main/kotlin/com/example/PruebaCrud/controller/ConferenceController.kt
@@ -1,5 +1,7 @@
 package com.example.PruebaCrud.controller
 
+
+
 import com.example.PruebaCrud.model.Conference
 import com.example.PruebaCrud.service.ConferenceService
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,16 +14,34 @@ import org.springframework.web.bind.annotation.*
 class ConferenceController {
     @Autowired
     lateinit var conferenceService: ConferenceService
+
     @GetMapping
-    fun list ():List <Conference>{
+    fun list ():List <Conference> {
         return conferenceService.list()
     }
+
+    @GetMapping("/{id}")
+    fun listById (@PathVariable("id") id: Long): ResponseEntity<*> {
+        return ResponseEntity(conferenceService.listById(id), HttpStatus.OK)
+    }
+
     @PostMapping
     fun save (@RequestBody conference: Conference): ResponseEntity<Conference> {
         return ResponseEntity(conferenceService.save(conference), HttpStatus.OK)
     }
+
     @PutMapping
     fun update (@RequestBody conference: Conference): ResponseEntity<Conference> {
         return ResponseEntity(conferenceService.update(conference), HttpStatus.OK)
+    }
+
+    @PatchMapping
+    fun updateName (@RequestBody conference: Conference): ResponseEntity<Conference> {
+        return ResponseEntity(conferenceService.updateName(conference), HttpStatus.OK)
+    }
+
+    @DeleteMapping("/conference/{id}")
+    fun delete (@PathVariable("id") id: Long):Boolean?{
+        return conferenceService.delete(id)
     }
 }
--- src/main/kotlin/com/example/PruebaCrud/service/ConferenceService.kt
+++ src/main/kotlin/com/example/PruebaCrud/service/ConferenceService.kt
@@ -2,8 +2,9 @@ package com.example.PruebaCrud.service
 
 
 
+
+
 import com.example.PruebaCrud.model.Conference
-import com.example.PruebaCrud.repository.AssistantRepository
 import com.example.PruebaCrud.repository.ConferenceRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
@@ -12,29 +13,22 @@ import org.springframework.web.server.ResponseStatusException
 
 @Service
 class ConferenceService {
-
-
     @Autowired
     lateinit var conferenceRepository: ConferenceRepository
 
-
-
-    fun list ():List<Conference>{
+    fun list(): List<Conference> {
         return conferenceRepository.findAll()
     }
 
-    fun save(conference: Conference): Conference{
+    fun save(conference: Conference): Conference {
         try {
-            conferenceRepository.findById(conference.id)
-                ?: throw Exception("Id del cliente no encontrada")
             return conferenceRepository.save(conference)
-        }catch (ex : Exception){
-            throw ResponseStatusException(
-                HttpStatus.NOT_FOUND, ex.message, ex)
+        } catch (ex: Exception) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, ex.message)
         }
     }
 
-    fun update(conference: Conference): Conference {
+    fun update(conference: Conference): Conference{
         try {
             conferenceRepository.findById(conference.id)
                 ?: throw Exception("ID no existe")
@@ -43,5 +37,30 @@ class ConferenceService {
         } catch (ex: Exception) {
             throw ResponseStatusException(HttpStatus.NOT_FOUND, ex.message)
         }
+    }
+
+    fun updateName(conference: Conference): Conference {
+        //try {
+        conferenceRepository.findById(conference.id)
+            ?: throw Exception("ID no existe")
+        return conferenceRepository.save(conference)
+        //} catch (ex: Exception) {
+        //   throw ResponseStatusException(HttpStatus.NOT_FOUND, ex.message)
+        //  }
+    }
+
+    fun delete(id: Long?): Boolean? {
+        try {
+            val response = conferenceRepository.findById(id)
+                ?: throw Exception("ID no existe")
+            conferenceRepository.deleteById(id!!)
+            return true
+        } catch (ex: Exception) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, ex.message)
+        }
+    }
+
+    fun listById(id: Long?): Conference? {
+        return conferenceRepository.findById(id)
     }
 }
